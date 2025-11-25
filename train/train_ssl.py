ad3787fbe1942e2068afe173a0e9e703209ee004
YujiaZhang123 <157528365+YujiaZhang123@users.noreply.github.com>
2025-11-24 20:34:07 -0500

    Update train_ssl.py

--- train/train_ssl.py
+++ train/train_ssl.py
@@ -43,8 +43,8 @@ class TrainingConfig:
     local_crops_scale: tuple = (0.05, 0.3)
 
     # ----- optimization -----
-    batch_size: int = 200             
-    num_workers: int = 24
+    batch_size: int = 250             
+    num_workers: int = 26
     epochs: int = 220
     base_lr: float = 2e-4
     min_lr: float = 2e-6
